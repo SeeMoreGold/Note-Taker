c228e488a1a571f7f402db567ed9cbc7058fbfc4
SeeMoreGold <moreau.cj@gmail.com>
2020-11-04 12:40:33 -0500

    HTML routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+
+const path = require("path");
+
+// ROUTES
+
+module.exports = function(app) {
+  // HTML GET Requests
+
+  app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // If no matching route is found default to the index file
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
